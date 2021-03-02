748427e0c9e7cd4a7c5ebafd923be4de76fc10a1
DevNetzwerkkabel <62692932+deNetzwerkkabel@users.noreply.github.com>
2021-03-02 11:20:40 +0100

    Xcode Projekt von DarkIserv

---
+++ DarkIServ/DarkIServ/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  DarkIServ
+//
+//  Created by System Administrator on 01.03.21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ DarkIServ/DarkIServ/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  DarkIServ
+//
+//  Created by System Administrator on 01.03.21.
+//
+
+import Cocoa
+import SafariServices.SFSafariApplication
+import SafariServices.SFSafariExtensionManager
+
+let appName = "DarkIServ"
+let extensionBundleIdentifier = "com.yourCompany.DarkIServ.Extension"
+
+class ViewController: NSViewController {
+
+    @IBOutlet var appNameLabel: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.appNameLabel.stringValue = appName
+        SFSafariExtensionManager.getStateOfSafariExtension(withIdentifier: extensionBundleIdentifier) { (state, error) in
+            guard let state = state, error == nil else {
+                // Insert code to inform the user that something went wrong.
+                return
+            }
+
+            DispatchQueue.main.async {
+                if (state.isEnabled) {
+                    self.appNameLabel.stringValue = "Die Dark IServ Erweiterung ist aktiviert und kann ab sofort verwendet werden"
+                } else {
+                    self.appNameLabel.stringValue = "Zum Abschließen dernstallation aktivieren Sie bitte das Dark IServ Plugin im Safari"
+                }
+            }
+        }
+    }
+    
+    @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
+        SFSafariApplication.showPreferencesForExtension(withIdentifier: extensionBundleIdentifier) { error in
+            guard error == nil else {
+                // Insert code to inform the user that something went wrong.
+                return
+            }
+
+            DispatchQueue.main.async {
+                NSApplication.shared.terminate(nil)
+            }
+        }
+    }
+
+}
